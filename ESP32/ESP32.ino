821bcf0d830213e7beece94a646e13c2fe087e07
Oleksandr Korablev <143433834+OleksandrKorablev@users.noreply.github.com>
2025-05-24 23:01:06 +0300

    Update ESP32.ino

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -37,21 +37,26 @@ void setup() {
   }
   Serial.println("SD card initialized successfully.");
 
-  // Перевірка наявності файлу /System/Authorization.html
+  // Перевірка наявності файлів
   if (SD.exists("/System/Authorization.html")) {
     Serial.println("File /System/Authorization.html found.");
   } else {
     Serial.println("File /System/Authorization.html not found.");
   }
+  if (SD.exists("/System/DataControllers.html")) {
+    Serial.println("File /System/DataControllers.html found.");
+  } else {
+    Serial.println("File /System/DataControllers.html not found.");
+  }
 
-  // Налаштування статичних маршрутів для ресурсів, що використовуються у сторінці
+  // Налаштування статичних маршрутів для ресурсів
   server.serveStatic("/styles", SD, "/System/styles");
   server.serveStatic("/js", SD, "/System/js");
   server.serveStatic("/libs", SD, "/System/libs");
   server.serveStatic("/Login_and_Password", SD, "/System/Login_and_Password");
   server.serveStatic("/DataFromMicrocontrollers", SD, "/System/DataFromMicrocontrollers");
 
-  // Налаштування кореневого маршруту для прямого відкриття Authorization.html
+  // Налаштування кореневого маршруту для Authorization.html (відразу відкривається)
   server.on("/", HTTP_GET, []() {
     if (SD.exists("/System/Authorization.html")) {
       File authFile = SD.open("/System/Authorization.html", FILE_READ);
@@ -65,6 +70,24 @@ void setup() {
       }
     } else {
       server.send(404, "text/html", "<h1>Authorization page not found!</h1>");
+    }
+  });
+
+  // Налаштування маршруту для DataControllers.html
+  // Коли користувач натискає кнопку "Увійти" на сторінці Authorization.html, браузер має відправити запит до /DataControllers.html
+  server.on("/DataControllers.html", HTTP_GET, []() {
+    if (SD.exists("/System/DataControllers.html")) {
+      File dcFile = SD.open("/System/DataControllers.html", FILE_READ);
+      if (dcFile) {
+        Serial.println("Streaming /System/DataControllers.html...");
+        server.streamFile(dcFile, "text/html");
+        dcFile.close();
+      } else {
+        Serial.println("Error opening /System/DataControllers.html!");
+        server.send(500, "text/html", "<h1>Error opening DataControllers.html file!</h1>");
+      }
+    } else {
+      server.send(404, "text/html", "<h1>DataControllers page not found!</h1>");
     }
   });
 
